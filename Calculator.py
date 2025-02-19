752bfacee1cee914277dd0bf8a3c194c5fb87598
Dv sathvik <sathvikdupaguntla2000@gmail.com>
2025-02-19 17:02:51 +0530

    This branch is to check the square root of a number

--- Calculator.py
+++ Calculator.py
@@ -12,6 +12,9 @@ class Calculator:
 
     def divide(self, a, b):
         return a / b
+    
+    def squareroot(self,a):
+        return math.sqrt(a)
 
 
 if __name__ == "__main__":
@@ -23,3 +26,4 @@ print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
 print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+print(f"The square root of {num1} is = {calculator.squareroot(num1)}")
