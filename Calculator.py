c04fa678e7da5199fc3140adf045b11bff685850
Dv sathvik <sathvikdupaguntla2000@gmail.com>
2025-02-19 17:48:18 +0530

    error

--- Calculator.py
+++ Calculator.py
@@ -14,9 +14,6 @@ class Calculator:
         if b == 0:
             raise ValueError("Cannot divide by Zero")
         return a / b
-    
-    def squareroot(self,a):
-        return math.sqrt(a)
 
 
 if __name__ == "__main__":
@@ -28,4 +25,3 @@ print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
 print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
-print(f"The square root of {num1} is = {calculator.squareroot(num1)}")
