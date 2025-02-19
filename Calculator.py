9ba8b09400db3ace966ef9b73be592acabdf086d
Dv sathvik <sathvikdupaguntla2000@gmail.com>
2025-02-19 18:04:37 +0530

    tested in dev with all the features working fine

--- Calculator.py
+++ Calculator.py
@@ -21,7 +21,7 @@ class Calculator:
 if __name__ == "__main__":
     calculator = Calculator()
     num1 = 16
-    num2 = 0
+    num2 = 1
 
 print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
